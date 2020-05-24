16d37b6a2923419ce9fa36bb4a1979174efd926c
Olubunmi Tosin <olubunmivictor6@gmail.com>
2020-05-24 03:02:51 +0100

    added travis

--- tests/py-57.sh
+++ tests/py-57.sh
@@ -4,7 +4,7 @@ cp composer.json original-composer.json
 
 sed '/    printerClass="Codedungeon\\PHPUnitPrettyResultPrinter\\Printer"/d' phpunit.xml > phpunit-57.xml
 composer remove codedungeon/phpunit-result-printer --no-update --dev
-composer require "orchestra/testbench:3.7.*" --no-update --dev
+composer require "orchestra/testbench:3.8.*" --no-update --dev
 composer require "phpunit/phpunit:8.*" --no-update --dev
 composer update --prefer-source --no-interaction
 
